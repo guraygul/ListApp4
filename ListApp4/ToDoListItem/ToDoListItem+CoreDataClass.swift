59b4fcda1bde88bf2cc00090b8cbb787d3ca28d6
Güray Gül <guraygul59@gmail.com>
2023-12-27 20:30:51 +0300

    Initial Commit

---
+++ ListApp4/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ListApp4
+//
+//  Created by Güray Gül on 26.12.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
